a5cc9511aa465c6370cf2d11287a709058614b2d
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-01-22 16:46:19 -0300

    Update Header.js
    Update on imports

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import styles from "../styles/Header.module.scss";
+import "../styles/Header.module.scss";
 
 const Header = () => {
   return (

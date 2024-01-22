26e762e8596b454f01ac688f63ac7202d4bf2861
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-01-22 16:42:06 -0300

    Header/Navbar Development Initiated: Initial Structure Added
    This commit marks the initiation of header development, with the addition of the initial structure. The focus is on establishing the foundational elements that will contribute to a cohesive and functional website header.

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,167 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import styles from "../styles/Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className="border-bottom fixed-top">
+      <nav className="navbar navbar-expand-md p-lg-0 pb-sm-0 p-0">
+          <Link
+            className="navbar-logo pb-sm-0 pt-sm-1 pt-1 pb-1"
+          >
+            <img
+              id="logo"
+              src="{{ url_for('static', filename='assets/svgs/logoonly.svg') }}"
+              alt="logo"
+            />
+          </Link>
+          <div className="mx-auto">
+            <div className="nav nav-tabs circle-tabs d-flex align-content-between circle-container">
+              <ul className="nav nav-tabs circle-tabs">
+                <li className="nav-item">
+                  <Link className="nav-link">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.5em"
+                      height="1.5em"
+                      fill="currentColor"
+                      className="bi bi-house"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+                    </svg>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.5em"
+                      height="1.5em"
+                      fill="currentColor"
+                      className="bi bi-houses"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M5.793 1a1 1 0 0 1 1.414 0l.647.646a.5.5 0 1 1-.708.708L6.5 1.707 2 6.207V12.5a.5.5 0 0 0 .5.5.5.5 0 0 1 0 1A1.5 1.5 0 0 1 1 12.5V7.207l-.146.147a.5.5 0 0 1-.708-.708zm3 1a1 1 0 0 1 1.414 0L12 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l1.854 1.853a.5.5 0 0 1-.708.708L15 8.207V13.5a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 4 13.5V8.207l-.146.147a.5.5 0 1 1-.708-.708zm.707.707L5 7.207V13.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5V7.207z" />
+                    </svg>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link disabled"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.5em"
+                      height="1.5em"
+                      fill="currentColor"
+                      className="bi bi-search"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                    </svg>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.5em"
+                      height="1.5em"
+                      fill="currentColor"
+                      className="bi bi-people"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                    </svg>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link disabled"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.5em"
+                      height="1.5em"
+                      fill="currentColor"
+                      className="bi bi-chat-dots"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+                      <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2" />
+                    </svg>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="ml-auto">
+            <div className="flex-shrink-0 dropdown">
+              <Link
+                className="d-block link-dark text-decoration-none dropdown-toggle"
+                id="dropdownUser2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="{{ current_user.profile.profile_image_path }}"
+                  alt="User profile"
+                  width="35em"
+                  height="35em"
+                  className="rounded-circle d-none d-block"
+                />
+              </Link>
+              <ul
+                className="dropdown-menu dropdown-menu-end text-small shadow"
+                aria-labelledby="dropdownUser2"
+              >
+                <li>
+                  <Link
+                    className="dropdown-item"
+                  >
+                    Create post
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item"
+                  >
+                    Create community
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item "
+                  >
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <Link className="dropdown-item disabled">
+                    Settings
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item"
+                  >
+                    Sign out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

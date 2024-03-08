c086d8ee71491a93dc342c4f429b342a99bb5427
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-08 15:10:45 -0300

    Create Feed.js

---
+++ social-frontend/src/Components/feed/Feed.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Dropdown, Button, Row, Col } from "react-bootstrap";
+
+// Import styles
+import styles from "./feed_styles.module.scss";
+
+const Feed = () => {
+  return (
+    <Row className={`mx-auto ${styles.feed}`}>
+      <Col className={`d-flex justify-content-center border-bottom pb-2 ${styles.feedHeader}`}>
+        <Button variant="dark">New Post</Button>
+
+        <Dropdown className="ms-auto">
+          <Dropdown.Toggle variant="dark" id="feed-dropdown">
+            Sort by
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>Recent</Dropdown.Item>
+            <Dropdown.Item>Recommended</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Col>
+    </Row>
+  );
+};
+
+export default Feed;

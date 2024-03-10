45c57578b5984ee7fc29e5640ccf6b65d98fd80e
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-10 16:24:56 -0300

    Update feed.js - Fix buttons position

--- social-frontend/src/Components/feed/Feed.js
+++ social-frontend/src/Components/feed/Feed.js
@@ -1,8 +1,30 @@
 import React from "react";
-import { Dropdown, Button, Row, Col } from "react-bootstrap";
+import { Dropdown, Button, Container, Row, Col } from "react-bootstrap";
 
 // Import styles
 import styles from "./feed_styles.module.scss";
+
+// Import Post component
+import Post from "../posts/Post";
+
+const FeedOptions = () => {
+  return (
+    <Col
+      className={`d-flex justify-content-center border-bottom pb-2 ${styles.feedHeader}`}
+    >
+      <Button variant="dark">New Post</Button>
+      <Dropdown className="ms-auto">
+        <Dropdown.Toggle variant="dark" id="feed-dropdown">
+          Sort by
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item>Recent</Dropdown.Item>
+          <Dropdown.Item>Recommended</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Col>
+  );
+};
 
 /**
  * Renders the feed component.
@@ -10,21 +32,17 @@ import styles from "./feed_styles.module.scss";
  */
 const Feed = () => {
   return (
-    <Row className={`mx-auto ${styles.feed}`}>
-      <Col className={`d-flex justify-content-center border-bottom pb-2 ${styles.feedHeader}`}>
-        <Button variant="dark">New Post</Button>
+    <Container className={`mx-auto ${styles.feed}`}>
+      {/* Feed options component */}
+        <FeedOptions/>
 
-        <Dropdown className="ms-auto">
-          <Dropdown.Toggle variant="dark" id="feed-dropdown">
-            Sort by
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item>Recent</Dropdown.Item>
-            <Dropdown.Item>Recommended</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Col>
-    </Row>
+      {/* Feed content here */}
+
+        <Post/>
+        <Post/>
+        <Post/>
+
+    </Container>
   );
 };
 
